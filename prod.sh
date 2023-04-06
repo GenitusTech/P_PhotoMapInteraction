6db4082e7983beb167cad99b787f8f763f0ff415
Genitus Tech <91600327+Eternal-Grace@users.noreply.github.com>
2023-04-06 21:27:59 +0200

    change mod on script

---
+++ prod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git pull
+npm install && npm run build
+composer install --prefer-dist --no-dev
+
+php artisan view:clear
+php artisan cache:clear
+
+php artisan view:cache
+php artisan route:cache
+php artisan config:cache
+
+php artisan clear-compiled
+
+php artisan optimize
+
+composer dumpautoload
